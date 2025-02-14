7c3c90bb514432172ce16125139fb15e1808ff90
Jacob Duba <jacob@mooshoe.me>
2025-02-14 12:38:07 -0600

    Scaffold files

--- CodeSearch/create_data.py
+++ CodeSearch/create_data.py
@@ -1,5 +1,8 @@
+# This file created a .npy file containing embeddings for each code example in a dataset.
+
 import torch
 from unixcoder import UniXcoder
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = UniXcoder("microsoft/unixcoder-base")
+model.to(device)
---
+++ CodeSearch/search.py
@@ -0,0 +1,8 @@
+# Waits for some text input. Returns similiar code snippets from the training data.
+
+import torch
+from unixcoder import UniXcoder
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = UniXcoder("microsoft/unixcoder-base")
+model.to(device)
