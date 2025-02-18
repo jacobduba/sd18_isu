2c3a0033164037677c63720ec5428dbf4cbd444c
VirgCuts <cvcuts@iastate.edu>
2025-02-18 01:11:19 -0600

    This reverts commit 35166ed4758e14e665088443c2c0ab5d875268e8.

--- CodeSearch/create_data.py
+++ CodeSearch/create_data.py
@@ -1,21 +1,8 @@
 # This file created a .npy file containing embeddings for each code example in a dataset.
 
 import torch
-#new dependency with datasets for huggingface
-#uv pip install datasets
-import datasets
 from unixcoder import UniXcoder
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = UniXcoder("microsoft/unixcoder-base")
 model.to(device)
-
-from datasets import load_dataset
-
-# Load the dataset
-#needed to add the trust setting as code_search required it, could be bad
-#seems to be needed to structure the dataset?
-#NOTICE THIS LOAD WILL DOWNLOAD THE DATASET LOCALLY!!! abt 3 gigs total
-dataset = load_dataset("code-search-net/code_search_net", trust_remote_code=True)
-#simply displays "datasplits" and each splits data fields 
-print(dataset)
