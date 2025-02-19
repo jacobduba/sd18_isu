887f8e8ea6ab76901384e3448cadab5de1071554
dieggomyeggo <diegoleonardopc@gmail.com>
2025-02-18 18:08:32 -0600

    addressing comments

--- CodeSearch/create_data.py
+++ CodeSearch/create_data.py
@@ -1,6 +1,6 @@
-from usecases import unixcoder
+from data_processing import process_data, create_code_search_net_dataset
 
 if __name__ == "__main__":
-    data_points = unixcoder.create_code_search_net_dataset()
+    data_points = create_code_search_net_dataset()
     if data_points:
-        unixcoder.process_data(data_points)
+        process_data(data_points)
