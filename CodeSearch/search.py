67895b8580feb89afe94943b6f1c35f6c1471bed
coshea <coshea@iastate.edu>
2025-03-04 00:01:50 -0600

    Addressing comments, updating json into Pairs and defining types

--- CodeSearch/search.py
+++ CodeSearch/search.py
@@ -2,10 +2,10 @@ from search_processing import get_processed_data, process_user_code_segment, get
 
 if __name__ == "__main__":
     user_input = input("Enter your code description: ")
-    code_segment_list, embedding_list, comment_string_list = get_processed_data()
+    processed_data = get_processed_data()
     processed_user_code = process_user_code_segment(user_input)
-    top_ten = get_top_ten(processed_user_code, embedding_list)
+    top_ten = get_top_ten(processed_user_code, processed_data)
     counter = 1
     for index, score in top_ten:
-        print(f"{counter}: Similarity score: {score} \n Comment string: {comment_string_list[index]} \n Code snippet: {code_segment_list[index]}")
+        print(f"{counter}: Similarity score: {score} \n Comment string: {processed_data[index].comment_string} \n Code snippet: {processed_data[index].code_string}")
         counter += 1
--- CodeSearch/search_processing.py
+++ CodeSearch/search_processing.py
@@ -4,26 +4,31 @@ import json
 import numpy as np
 import torch
 from typing import List, Tuple
+from models.unix_coder import Pair
 from unixcoder import UniXcoder
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = UniXcoder("microsoft/unixcoder-base")
 
 
-def get_processed_data() -> Tuple[List[str], np.ndarray, List[str]]:
+def get_processed_data() -> List[Pair]:
     try:
         with open('data.json', 'r') as file:
             # contains objects that have an array of numbers and a code string
             json_data = json.load(file)
 
             # get the code strings and embeddings from json.data
-            return [item["code_string"] for item in json_data], np.array([item["comment_embedding"] for item in json_data]), [item["comment_string"] for item in json_data]
+            validated_pairs = []
+            for item in json_data:
+                validated_pairs.append(Pair.model_validate(item))
+            
+            return validated_pairs
     except FileNotFoundError:
         print("The data.json file does not exist")
     except json.JSONDecodeError:
         print("Error decoding data.json")
 
-    return [], np.array([]), []
+    return []
 
 
 def process_user_code_segment(user_input: str) -> List[float]:
@@ -33,14 +38,14 @@ def process_user_code_segment(user_input: str) -> List[float]:
     _, nl_embedding = model(source_ids)
 
     # Normalize embedding and flatten it
-    return torch.flatten(torch.nn.functional.normalize(torch.tensor(nl_embedding), p=2, dim=1)).tolist()
+    return torch.flatten(torch.nn.functional.normalize(nl_embedding.clone().detach(), p=2, dim=1)).tolist()
 
 
-def get_top_ten(processed_user_code: List[float], embedding_list: np.ndarray) -> list:
+def get_top_ten(processed_user_code: List[float], processed_data: List[Pair]) -> List[Tuple[int, float]]:
     code_segment_map = {}
-    for index, vector in enumerate(embedding_list):
+    for index, pair in enumerate(processed_data):
         # calculate similarity with the dot product and store the value
-        code_segment_map[index] = np.dot(vector, processed_user_code)
+        code_segment_map[index] = np.dot(pair.comment_embedding, processed_user_code)
 
     # get top 10 most similar code segments
     return sorted(code_segment_map.items(), key=lambda x: x[1], reverse=True)[:10]
